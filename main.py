4f4342109954520bb76ac321b1f20620ac4dcdbb
Les Peters <les.peters@gmail.com>
2021-04-24 16:59:13 -0400

    1: suppress

--- main.py
+++ main.py
@@ -13,26 +13,25 @@ menu = [
     'Init'
 ]
 
-menu_selection = 0
+menu_selection = [ 0 ]
 menu_depth = 0
 
-a = 0
 
 def on_down():
     global menu_selection
-    if menu_selection == len(menu) - 1:
-        menu_selection = 0
+    if menu_selection[menu_depth] == len(menu) - 1:
+        menu_selection[menu_depth] = 0
     else:
-        menu_selection += 1
-    print(menu[menu_selection])
+        menu_selection[menu_depth] += 1
+    print(menu[menu_selection[menu_depth]])
 
 def on_up():
     global menu_selection
-    if menu_selection == 0:
-        menu_selection = len(menu) - 1
+    if menu_selection[menu_depth] == 0:
+        menu_selection[menu_depth] = len(menu) - 1
     else:
-        menu_selection -= 1
-    print(menu[menu_selection])
+        menu_selection[menu_depth] -= 1
+    print(menu[menu_selection[menu_depth]])
 
 def on_press(key):
     global menu_selection
@@ -42,11 +41,11 @@ def on_press(key):
     elif key == keyboard.Key.down:
         on_down()
     elif key == keyboard.Key.enter:
-        print('selected', menu[menu_selection])
+        print('selected', menu[menu_selection[menu_depth]])
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as l:
+with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as l:
     l.join()
