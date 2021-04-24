3114f8bff2df8a0839773d04622d61e0194f99e0
Les Peters <les.peters@gmail.com>
2021-04-24 19:07:57 -0400

    1:no

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from roll import roll
 from pynput import keyboard
 
 menu = {
-    '0': 'Check',
-    '1': 'Save',
-    '2': 'Melee',
-    '3': 'Range',
-    '4': 'Cast',
-    '5': 'Rest',
-    '6': 'Init'
+    '0': { 'name': 'Check' },
+    '1': { 'name': 'Save' },
+    '2': { 'name': 'Melee' },
+    '3': { 'name': 'Range' },
+    '4': { 'name': 'Cast' },
+    '5': { 'name': 'Rest' },
+    '6': { 'name': 'Init' }
 }
 
 menu_selection = [ '0' ]
@@ -23,7 +23,7 @@ def on_down():
     else:
         menu_selection[menu_depth] = str(int(menu_selection[menu_depth]) + 1)
     # print(type(menu[menu_selection[menu_depth]]))
-    print(menu[menu_selection[menu_depth]])
+    print(menu[menu_selection[menu_depth]]['name'])
 
 def on_up():
     global menu_selection
@@ -32,7 +32,7 @@ def on_up():
     else:
         menu_selection[menu_depth] = str(int(menu_selection[menu_depth]) - 1)
     # print(type(menu[menu_selection[menu_depth]]))
-    print(menu[menu_selection[menu_depth]])
+    print(menu[menu_selection[menu_depth]]['name'])
 
 def on_press(key):
     global menu_selection
@@ -42,7 +42,7 @@ def on_press(key):
     elif key == keyboard.Key.down:
         on_down()
     elif key == keyboard.Key.enter:
-        print('selected', menu[menu_selection[menu_depth]])
+        print('selected', menu[menu_selection[menu_depth]]['name'])
 
 def on_release(key):
     if key == keyboard.Key.esc:
