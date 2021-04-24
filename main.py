4d1677fb197e10899a5f3531d1f8dde4ab74fa1c
Les Peters <les.peters@gmail.com>
2021-04-24 17:29:06 -0400

    1: string menu key

--- main.py
+++ main.py
@@ -3,34 +3,35 @@
 from roll import roll
 from pynput import keyboard
 
-menu = [
-    'Check',
-    'Save',
-    'Melee',
-    'Range',
-    'Cast',
-    'Rest',
-    'Init'
-]
+menu = {
+    '0': 'Check',
+    '1': 'Save',
+    '2': 'Melee',
+    '3': 'Range',
+    '4': 'Cast',
+    '5': 'Rest',
+    '6': 'Init'
+}
 
-menu_selection = [ 0 ]
+menu_selection = [ '0' ]
 menu_depth = 0
-
 
 def on_down():
     global menu_selection
-    if menu_selection[menu_depth] == len(menu) - 1:
-        menu_selection[menu_depth] = 0
+    if menu_selection[menu_depth] == str(len(menu) - 1):
+        menu_selection[menu_depth] = '0'
     else:
-        menu_selection[menu_depth] += 1
+        menu_selection[menu_depth] = str(int(menu_selection[menu_depth]) + 1)
+    # print(type(menu[menu_selection[menu_depth]]))
     print(menu[menu_selection[menu_depth]])
 
 def on_up():
     global menu_selection
-    if menu_selection[menu_depth] == 0:
-        menu_selection[menu_depth] = len(menu) - 1
+    if menu_selection[menu_depth] == '0':
+        menu_selection[menu_depth] = str(len(menu) - 1)
     else:
-        menu_selection[menu_depth] -= 1
+        menu_selection[menu_depth] = str(int(menu_selection[menu_depth]) - 1)
+    # print(type(menu[menu_selection[menu_depth]]))
     print(menu[menu_selection[menu_depth]])
 
 def on_press(key):
