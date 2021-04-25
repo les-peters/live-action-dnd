c2f32bfdf291bf236cbfc1b7c2fbe960921162e8
Les Peters <les.peters@gmail.com>
2021-04-25 19:23:22 -0400

    1:adv/dis rolls

--- roll.py
+++ roll.py
@@ -5,3 +5,20 @@ import random
 def roll(d):
     roll = int(random.random() * d) + 1
     return roll
+
+def adv(d):
+    roll1 = roll(d)
+    roll2 = roll(d)
+    if roll1 > roll2:
+        return roll1
+    else:
+        return roll2
+
+def dis(d):
+    roll1 = roll(d)
+    roll2 = roll(d)
+    if roll1 < roll2:
+        return roll1
+    else:
+        return roll2
+
